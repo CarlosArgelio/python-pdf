933f1cb62897a136fe9eba9d771e3d5559bca57c
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-10-11 09:42:04 -0400

    fix: add entryPoint code generate pdf with pdfkit

--- libraries/pdfkit/basic/generate_pdf.py
+++ libraries/pdfkit/basic/generate_pdf.py
@@ -8,4 +8,8 @@ def generate_pdf(_from: str):
     elif _from == 'url':
         pdfkit.from_url('http://micropyramid.com', 'micro.pdf')
     elif _from == 'file':
-        pdfkit.from_file('micropyramid.html', 'micro.pdf')
+        pdfkit.from_file('templates/dinamics/basic.html', 'micro.pdf')
+
+if __name__ == '__main__':
+    arg: str = sys.argv[1]
+    generate_pdf(arg)
